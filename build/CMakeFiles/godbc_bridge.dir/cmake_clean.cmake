1a946a50e48dce47649029ce753ec74c2b15a829
David Sandor <david.sandor@gmail.com>
2025-03-20 14:34:32 -0400

    Remove build directory from git tracking and update .gitignore

--- build/CMakeFiles/godbc_bridge.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/godbc_bridge"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/godbc_bridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/godbc_bridge.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for godbc_bridge.
-# This may be replaced when dependencies are built.
